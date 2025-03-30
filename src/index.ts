a77591d80cc94e71c3b1d36032c243e1e61319cb
gmegidish <gil@megidish.net>
2025-03-30 17:23:20 +0200

    fix: added shebang

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { createMcpServer } from "./server";
 import { error } from "./logger";
